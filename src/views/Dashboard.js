3f06b26ab60f7145aee3575649aeec4928d47142
PrashantBtkl <prashant44backup@gmail.com>
2022-05-02 15:53:29 +0530

    add map to select webpage image on uuid

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,6 +1,9 @@
  import React from "react";
  import h337 from "heatmap.js";
- import webpage from "assets/img/91092828-2644-4d9d-b0a2-09f06f23f6fd.png";
+ import l_webpage from "assets/img/91092828-2644-4d9d-b0a2-09f06f23f6fd.png";
+ import gh_webpage from "assets/img/f8eccf87-9b5d-4f24-9032-64e67e783d51.png";
+ import defaultImg from "assets/img/default.jpg";
+ import index from "assets/img/loading-bubbles.svg";
 // react-bootstrap components
 import {
   Badge,
@@ -17,6 +20,12 @@ import {
   Tooltip,
   Image,
 } from "react-bootstrap";
+
+const imageMap = new Map();
+var imageSrc = defaultImg;
+imageMap.set("f8eccf87-9b5d-4f24-9032-64e67e783d51", gh_webpage);
+imageMap.set("91092828-2644-4d9d-b0a2-09f06f23f6fd", l_webpage);
+
 class Dashboard extends React.Component {
   constructor(props){
     super(props);
@@ -30,8 +39,9 @@ class Dashboard extends React.Component {
   handleSubmit = (event) => {
     this.setState({page_id: this.element.value}, () =>
      console.log(this.state.page_id));
+     imageSrc = imageMap.get(this.state.page_id);
 
-    fetch(`http://127.0.0.1:5000/get_xy/${this.element.value}`)
+    fetch(`https://heatmap-api.herokuapp.com//get_xy/${this.element.value}`)
         .then((res) => res.json())
         .then((data) => {
           this.setState({ xy : data }, () => {
@@ -83,9 +93,16 @@ render() {
       <Row>
       <Col lg="15" sm="15">
             <Card>
+              <Card.Header>
+                <div className="legend">
+                  <i className="fas fa-circle text-info"></i>  Low&nbsp; &nbsp; 
+                  <i className="fas fa-circle text-success"></i>  Moderate&nbsp; &nbsp; 
+                  <i className="fas fa-circle text-danger"></i>  High&nbsp; &nbsp; 
+                </div>
+              </Card.Header>
               <Card.Body>
                 <div className="heatmap" fluid>
-                  <Image src={webpage} alt="webpage image"/>
+                  <Image src={imageSrc} alt="webpage image"/>
                 </div>
               </Card.Body>
             </Card>
